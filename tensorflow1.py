bfcaf86fd9b2fdd10e0d01ef29c532be44cb4769
KRDodge <20012011ryu@gmail.com>
2025-06-23 17:28:31 +0900

    6/23 선형 회귀

--- tensorflow1.py
+++ tensorflow1.py
@@ -16,3 +16,11 @@ print("node3:", node3)
 sess = tf.Session()
 print("sess.run(node1, node2):", sess.run([node1, node2]))
 print("sess.run(node3):", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+
+adder_node = a + b
+
+print(sess.run(adder_node, feed_dict = {a:3, b:4.5}))
+print(sess.run(adder_node, feed_dict = {a:[1,3], b:[2,4]}))
